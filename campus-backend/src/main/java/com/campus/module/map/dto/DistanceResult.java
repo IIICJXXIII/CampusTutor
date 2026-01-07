bc496326086a774a69ed836415933abc19341499
IIICJXXIII <venriacomt71@gmail.com>
2026-01-07 10:20:47 +0800

    修复了后端的问题

--- campus-backend/src/main/java/com/campus/module/map/dto/DistanceResult.java
+++ campus-backend/src/main/java/com/campus/module/map/dto/DistanceResult.java
@@ -24,6 +24,22 @@ public class DistanceResult {
      */
     private Result result;
 
+    /**
+     * 高德地图兼容 - 直接存储元素列表
+     */
+    private List<Element> elements;
+
+    public void setElements(List<Element> elements) {
+        this.elements = elements;
+        // 同时设置兼容的result结构
+        if (this.result == null) {
+            this.result = new Result();
+        }
+        DistanceElement de = new DistanceElement();
+        de.setElements(elements);
+        this.result.setRows(List.of(de));
+    }
+
     @Data
     public static class Result {
         /**
