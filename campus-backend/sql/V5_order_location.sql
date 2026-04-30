a2d802685f5547afb0fd426e27eb64e0abf6042e
inclementman <1761196827@qq.com>
2026-05-01 02:51:58 +0800

    feat: 清理了一下sql文件夹

--- campus-backend/sql/V5_order_location.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE `course_order`
-  ADD COLUMN `longitude` decimal(10, 7) DEFAULT NULL COMMENT '经度',
-  ADD COLUMN `latitude` decimal(10, 7) DEFAULT NULL COMMENT '纬度',
-  ADD COLUMN `address` varchar(255) DEFAULT NULL COMMENT '地址';
-
-UPDATE `course_order` o
-  INNER JOIN `demand_post` d ON o.demand_id = d.id
-  SET o.longitude = d.longitude, o.latitude = d.latitude, o.address = d.address
-  WHERE o.demand_id IS NOT NULL AND d.longitude IS NOT NULL;
